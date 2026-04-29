2c5fbbf9266d895d479edbf0822823ec63cafebe
Mollyiard <Idoeli2907@gmail.com>
2026-04-29 19:49:10 -0400

    Fixdagap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ export default function Navbar() {
     const section = document.querySelector(href)
     if (!section) return
 
-    const navbarOffset = 70
+    const navbarOffset = 50
 
     const y =
       section.getBoundingClientRect().top +
